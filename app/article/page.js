a524388bdeeb36c7cfc14a6f36dc562707485df4
TAMARA Forgive <ej613116@gmail.comgit config --global user.name TAMARA>
2024-11-08 10:35:15 +0100

    j'ai fait la structure du projet niveau1

---
+++ app/article/page.js
@@ -0,0 +1,8 @@
+export default function article (){
+    return (
+        <div>
+            <h1>Welcome to the OloStore</h1>
+            <p>Here you can find all your favorite products, from clothing to electronics, all at the best prices.</p>
+        </div>
+    )
+}
