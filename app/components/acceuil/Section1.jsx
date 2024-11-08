5057c0b00f9104e08aa26c38c4d4a404bae0af4e
ASSEKO <assekoazareel222@gmail.com>
2024-11-08 16:55:18 +0100

    section promesse ajouter

--- app/components/acceuil/Section1.jsx
+++ app/components/acceuil/Section1.jsx
@@ -6,11 +6,73 @@ export function Section1() {
       <div className={styles.section1}>
         <div className={styles.left}>
           <div className={styles.titre}>O'LOSTORE</div>{" "}
-          <div className={styles.para}>Vos articles <br />à porter <br /> de main</div>
+          <div className={styles.para}>
+            Vos articles <br />à porter <br /> de main
+          </div>
           <button className={styles.button}>commander</button>
         </div>{" "}
-        <div className={styles.right}>
-          
+        <div className={styles.right}></div>
+      </div>
+      <div className={styles.promesse}>
+        <div className={styles.cnt1}>
+        <div className={styles.promesse1}>
+          {" "}
+          <Image
+            src="/assets/livraison.png"
+            alt="Search Icon"
+            width={42}
+            height={42}
+            className={styles.Image}
+          />{" "}
+          <div className={styles.text}>
+            Livraison gratuite <br />
+            +15 000 XAF
+          </div>
+        </div>
+        <div className={styles.promesse1}>
+          {" "}
+          <Image
+            src="/assets/remboursement.png"
+            alt="Search Icon"
+            width={42}
+            height={42}
+            className={styles.Image}
+          />{" "}
+          <div className={styles.text}>
+            100 % rembourser <br />
+            10 jours
+          </div>
+        </div>
+        </div>
+        <div className={styles.cnt2}>
+        <div className={styles.promesse1}>
+          {" "}
+          <Image
+            src="/assets/time.png"
+            alt="Search Icon"
+            width={42}
+            height={42}
+            className={styles.Image}
+          />{" "}
+          <div className={styles.text}>
+            Livraison gratuite <br />
+            +15 000 XAF
+          </div>
+        </div>
+        <div className={styles.promesse1}>
+          {" "}
+          <Image
+            src="/assets/Container.png"
+            alt="Search Icon"
+            width={42}
+            height={42}
+            className={styles.Image}
+          />{" "}
+          <div className={styles.text}>
+            100 % rembourser <br />
+            10 jours
+          </div>
+        </div>
         </div>
       </div>
     </div>
