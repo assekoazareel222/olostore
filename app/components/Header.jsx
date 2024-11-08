e940b67afa358ab9b9c58d5d2ebf99ffe888c160
TAMARA Forgive <ej613116@gmail.comgit config --global user.name TAMARA>
2024-11-08 14:03:07 +0100

    j'ai fait le header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,53 @@
-export function Header (){
-    return (
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <div className="w-full">
+      <header className="w-[90%] m-auto py-1 md:py-2 bg-[#3883A2] rounded-lg mt-4 md:pr-6 pr-3 justify-between flex items-center">
         <div>
-            <h1>Welcome to the OloStore</h1>
-            <p>Here you can find all your favorite products, from clothing to electronics, all at the best prices.</p>
+          <Image
+            className="dark:invert w-20 md:w-30"
+            src="../assets/logo.svg"
+            alt="logo"
+            width={100}
+            height={100}
+            //   priority
+          />
         </div>
-    )
+        {/* <div>
+          <input placeholder="Rechercher vos produits" className="rounded-md py-1 md:py-3"> 
+          </input>
+        </div> */}
+        <div className="relative flex items-center">
+        <input
+            type="text"
+            placeholder="Rechercher vos produits"
+            className="md:pl-4 pl-1 md:pr-32 md:py-2 py-1 pr-1 border rounded-md w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <Image
+            src="../assets/recherche.svg"
+            alt="Search Icon"
+            width={22}
+            height={22}
+            className="absolute md:left-[302px] left-[180px] md:w-8 h-5 w-5 md:h-8 text-gray-400"
+          />
+        </div>
+        <div className="md:hidden lg:hidden">
+          <Image
+            className="md:hidden"
+            src="../assets/burger.svg"
+            alt="log"
+            width={22}
+            height={22}
+          />
+        </div>
+        <div className="flex gap-10 max-sm:hidden">
+          <p className="text-white text-sm max-sm:hidden md:text-lg">Acceuil</p>
+          <p className="text-white text-sm max-sm:hidden md:text-lg">
+            Produits
+          </p>
+        </div>
+      </header>
+    </div>
+  );
 }
--- app/produit/page.js
+++ app/produit/page.js
@@ -1,6 +1,9 @@
+import { Header } from "../components/Header";
+
 export default function formulaire (){
     return (
         <div>
+            <Header />
             <h1>Welcome to the OloStore</h1>
             <p>Here you can find all your favorite products, from clothing to electronics, all at the best prices.</p>
         </div>
