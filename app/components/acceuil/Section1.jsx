05a3abb17d145571b7cebdf8d764eb47d8a2d357
ASSEKO <assekoazareel222@gmail.com>
2024-11-08 15:03:28 +0100

    section1

--- app/components/acceuil/Section1.jsx
+++ app/components/acceuil/Section1.jsx
@@ -1,8 +1,18 @@
-export function Section1 (){
-    return (
-        <div>
-            <h1>Welcome to the OloStore</h1>
-            <p>Here you can find all your favorite products, from clothing to electronics, all at the best prices.</p>
+import styles from "../../style/section1.module.css";
+import Image from "next/image";
+export function Section1() {
+  return (
+    <div className={styles.section}>
+      <div className={styles.section1}>
+        <div className={styles.left}>
+          <div className={styles.titre}>O'LOSTORE</div>{" "}
+          <div className={styles.para}>Vos articles <br />à porter <br /> de main</div>
+          <button className={styles.button}>commander</button>
+        </div>{" "}
+        <div className={styles.right}>
+          
         </div>
-    )
+      </div>
+    </div>
+  );
 }
