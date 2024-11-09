be402013688a4262051055afba0d8cbd6e13d049
ASSEKO <assekoazareel222@gmail.com>
2024-11-09 20:14:56 +0100

    recuperation de la version 1.0

--- app/components/acceuil/Section2.jsx
+++ app/components/acceuil/Section2.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Link from "next/link";
 
 export function Section2() {
@@ -199,6 +200,128 @@ export function Section2() {
 
   
 
+=======
+import styles from "../../style/section2.module.css";
+import Image from "next/image";
+export function Section2() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.section2}>
+        <button className={styles.button}>Les bonnes affaires</button>
+        <div className={styles.produit1}>
+          <div className={styles.produitA}>
+            <Image
+              src="/assets/1372-samsung-galaxy-a55-5g-8-256gb-azul-libre-review-removebg-preview 2.svg"
+              alt="Search Icon"
+              width={300}
+              height={300}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+          <div className={styles.produitB}>
+            <Image
+              src="/assets/vélo.svg"
+              alt="Search Icon"
+              width={400}
+              height={400}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+          <div className={styles.produitC}>
+            <Image
+              src="/assets/ordinateurrouge.svg"
+              alt="Search Icon"
+              width={400}
+              height={400}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+        </div>
+        <div className={styles.produit2}>
+          <div className={styles.produitD}>
+            <Image
+              src="/assets/ordinateurrouge.svg"
+              alt="Search Icon"
+              width={400}
+              height={400}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+          <div className={styles.produitE}>
+            <Image
+              src="/assets/ordinateurrouge.svg"
+              alt="Search Icon"
+              width={400}
+              height={400}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+          <div className={styles.produitF}>
+            <Image
+              src="/assets/ordinateurrouge.svg"
+              alt="Search Icon"
+              width={400}
+              height={400}
+              className={styles.Image1}
+            />
+            <p className={styles.nom}>
+              Telephone <br /> Samsung A55
+            </p>
+            <div className={styles.prix}>
+              {" "}
+              <p className={styles.prix1}>200.000</p>{" "}
+              <p className={styles.prix2}>220.000</p>{" "}
+            </div>
+            <button className={styles.button}>commander</button>
+          </div>
+        </div>
+      </div>
+>>>>>>> 6a6f018 (debut de la section 2)
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import { Section1 } from "./components/acceuil/Section1";
 import { Header } from "./components/Header";
+<<<<<<< HEAD
 import { Cat } from "./components/produits/Cat";
 // import { Articles: } from "./components/produits/Articles";
 import { Section2 } from "./components/acceuil/Section2";
 import { Ogou } from "./components/acceuil/Ogou";
 import { Footer } from "./components/Footer";
 
+=======
+import { Section2 } from "./components/acceuil/Section2";
+>>>>>>> 6a6f018 (debut de la section 2)
 export default function Home() {
   return (<>
     <Header/>
     
     <Section1/>
+<<<<<<< HEAD
     <Cat />
     <Section2 />
     {/* <Articles /> */}
     <Ogou />
     <Footer />
+=======
+    <Section2/>
+>>>>>>> 6a6f018 (debut de la section 2)
     </>
   );
 }
