7590f376afafd597496bdae3a5e8c0e5b6130a05
ASSEKO <assekoazareel222@gmail.com>
2024-11-10 02:46:53 +0100

    index

--- app/components/acceuil/Section2.jsx
+++ app/components/acceuil/Section2.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import styles from "../../style/section2.module.css";
-import Image from "next/image";
-export function Section2() {
-  return (
-    <div className={styles.card}>
-      <div className={styles.section2}>
-        <button className={styles.button}>Les bonnes affaires</button>
-        <div className={styles.produit1}>
-          <div className={styles.produitA}>
-            <Image
-              src="/assets/1372-samsung-galaxy-a55-5g-8-256gb-azul-libre-review-removebg-preview 2.svg"
-              alt="Search Icon"
-              width={300}
-              height={300}
-              className={styles.Image1}
-            />
-            <p className={styles.nom}>
-              Telephone <br /> Samsung A55
-            </p>
-            <div className={styles.prix}>
-              {" "}
-              <p className={styles.prix1}>200.000</p>{" "}
-              <p className={styles.prix2}>220.000</p>{" "}
-=======
 import Link from "next/link";
 
 export function Section2() {
@@ -125,7 +100,6 @@ export function Section2() {
                 alt="Téléphone Samsung A55"
                 class="w-30 h-60 "
               />
->>>>>>> d33d673 (refonte de la page acceuil)
             </div>
             <div class="flex justify-end mr-8 flex-col">
               <p class="text-black  w-[30%] font-bold min-300:max-600:w-[100%]">Ecouteurs</p>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import { Section1 } from "./components/acceuil/Section1";
 import { Header } from "./components/Header";
-<<<<<<< HEAD
-import { Section2 } from "./components/acceuil/Section2";
-=======
 import { Cat } from "./components/produits/Cat";
 import { Section3 } from "./components/acceuil/Section3";
 // import { Articles: } from "./components/produits/Articles";
@@ -11,23 +8,18 @@ import { Section2 } from "./components/acceuil/Section2";
 import { Ogou } from "./components/acceuil/Ogou";
 import { Footer } from "./components/Footer";
 
->>>>>>> d33d673 (refonte de la page acceuil)
 export default function Home() {
   return (<>
           
     
     <Header/>
     <Section1/>
-<<<<<<< HEAD
-    <Section2/>
-=======
     <Cat />
     <Section2 />
     {/* <Articles /> */}
     <Ogou />
     <Section3 />
     <Footer />
->>>>>>> d33d673 (refonte de la page acceuil)
     </>
   );
 }
