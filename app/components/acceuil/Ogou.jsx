143e6a663ab28abf26fb650f8561e42d6c3d85e8
ASSEKO <assekoazareel222@gmail.com>
2024-11-10 03:21:51 +0100

    restauration de la pages acceuil

--- app/components/acceuil/Ogou.jsx
+++ app/components/acceuil/Ogou.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 export function Ogou(){
     return (
-        <div className="w-full mt-4 mb-6">
+        <div className="w-[90%] flex justify-end mt-4 mb-6">
             <Image
-            className=" w-[90%] m-auto h-[30%] "
+            className=" w-[90%] ml-52 m-auto h-[30%] "
             src="../assets/ogou.svg"
             alt="log"
             width={24}
